4b093957f66419ef750686faa73599900edad476
ShadowSlayer03 <sslayer441@gmail.com>
2024-09-14 11:36:02 +0530

    Added required hooks

---
+++ lib/auth.ts
@@ -0,0 +1,73 @@
+import * as SecureStore from "expo-secure-store";
+import * as Linking from "expo-linking";
+import { fetchAPI } from "./fetch";
+
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      const item = await SecureStore.getItemAsync(key);
+      if (item) {
+        console.log(`${key} was used 🔐 \n`);
+      } else {
+        console.log("No values stored under key: " + key);
+      }
+      return item;
+    } catch (error) {
+      console.error("SecureStore get item error: ", error);
+      await SecureStore.deleteItemAsync(key);
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
+export default tokenCache;
+
+export const googleOAuth = async (startOAuthFlow: any) => {
+  try {
+    const { createdSessionId, signUp, setActive } = await startOAuthFlow({
+      redirectUrl: Linking.createURL("/(root)/(tabs)/home", {
+        scheme: "myapp",
+      }),
+    });
+
+    if (createdSessionId) {
+      if (setActive) await setActive({ session: createdSessionId });
+
+      if (signUp.createdUserId) {
+        await fetchAPI("/(api)/user", {
+          method: "POST",
+          body: JSON.stringify({
+            name: `${signUp.firstName} ${signUp.lastName}`,
+            email: signUp.emailAddress,
+            clerkId: signUp.createdUserId,
+          }),
+        });
+
+        return {
+          success: true,
+          code: "Success",
+          message: "Authentication Successful!",
+        };
+      }
+
+      return {
+        success: false,
+        code: "Error",
+        message: "Some error occurred!",
+      };
+    }
+  } catch (error: any) {
+    return {
+      success: false,
+      code: error.code,
+      message: error.errors[0].longMessage,
+    };
+  }
+};
